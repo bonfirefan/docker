88d6a8a2e149b8c27253df3927cac9ccb90df502
Bonnie Fan <bonnie.y.fan@gmail.com>
2019-07-12 03:30:35 -0400

    changed for setup

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+
+# install pelias script
+ln -s "$(pwd)/pelias" /usr/local/bin/pelias
+
+#cwd
+cd projects/boston-city
+
+# configure environment 
+sed -i '/DATA_DIR/d' .env
+echo 'DATA_DIR=/data' >> .env
+
+# run build
+pelias compose pull
+pelias elastic start
+pelias elastic wait
+pelias elastic create
+pelias download all
+pelias prepare all
+pelias import all
+pelias compose up
